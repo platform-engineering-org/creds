45c345910f0d40d84a51a89ae1c7321e58bcb4ff
Liora Milbaum <lmilbaum@redhat.com>
2022-12-29 15:41:20 +0200

    feat: Storing auditing info in db

--- infra/lambda/lambda_function.py
+++ infra/lambda/lambda_function.py
@@ -10,12 +10,30 @@ def lambda_handler(event, context):
     yesterday = today - timedelta(days=1)
 
     client = boto3.client("cloudtrail")
-    eds_id = event["eds-urn"].replace('"', "").split("/")[1]
+    eds_id = event["eds-arn"].replace('"', "").split("/")[1]
 
     query = (
-        "SELECT userIdentity.arn AS Name, userIdentity.type AS Type, eventName"
-        f" FROM {eds_id} WHERE eventTime > '{yesterday} 00:00:00'"
-        f" AND eventTime < '{today} 00:00:00'"
+        "SELECT userIdentity.arn AS Name, eventName "
+        f"FROM {eds_id} "
+        f"WHERE userIdentity.type='IAMUser' "
+        f"AND eventTime > '{yesterday} 00:00:00' "
+        f"AND eventTime < '{today} 00:00:00'"
     )
     response = client.start_query(QueryStatement=query)
-    print(response)
+    query_id = response["QueryId"]
+    while True:
+        response = client.get_query_results(
+            EventDataStore=eds_id, QueryId=query_id
+        )
+        if response["QueryStatus"] == "FINISHED":
+            break
+    client = boto3.client("dynamodb")
+    response = client.put_item(
+        TableName=f"{event['table']}",
+        Item={
+            "date": {"S": str(yesterday)},
+            "num": {
+                "S": str(response["QueryStatistics"]["TotalResultsCount"])
+            },
+        },
+    )
